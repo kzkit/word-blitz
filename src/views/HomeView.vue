fe3c585e853d20b0213de1d8241cf318852604ad
Zhen Kit Kong <zhenkit.kong@petronas.com>
2022-04-03 22:27:18 +0800

    added main screen text

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,9 @@
 <script setup></script>
 
 <template>
-	<main>main page</main>
+	<main class="mx-auto">
+		<h1 class="text-8xl">Welcome to Word Blitz</h1>
+		<p>Challenge your friends to a battle of wits!</p>
+		<button>Get Started</button>
+	</main>
 </template>
