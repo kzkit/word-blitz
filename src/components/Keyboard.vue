03d40ce751052233720f242086fe92adb23f2ba9
Zhen Kit Kong <zhenkit.kong@petronas.com>
2022-04-03 22:26:28 +0800

    added keyboard component

---
+++ src/components/Keyboard.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="keyrow">
+        <div class="key" v-for="key in keyRow1" :key="key">
+            <button>{{ key }}</button>
+        </div>
+    </div>
+    <div class="keyrow">
+        <div class="key" v-for="key in keyRow2" :key="key">
+            <button>{{ key }}</button>
+        </div>
+    </div>
+    <div class="keyrow">
+        <div class="key" v-for="key in keyRow3" :key="key">
+            <button>{{ key }}</button>
+        </div>
+    </div>
+</template>
+<script setup>
+var keyRow1 = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p']
+var keyRow2 = ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l']
+var keyRow3 = ['z', 'x', 'c', 'v', 'b', 'n', 'm']
+
+
+
+</script>
+<style>
+.keyrow {
+    display: flex;
+}
+</style>
