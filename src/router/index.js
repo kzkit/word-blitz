ac84c45c236decfe1b008911715735a3739a0ef8
Zhen Kit Kong <zhenkit.kong@petronas.com>
2022-04-03 22:26:55 +0800

    init game route

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,22 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import Game from "../views/Game.vue";
 
 const router = createRouter({
-	history: createWebHistory(import.meta.env.BASE_URL),
-	routes: [
-		{
-			path: '/',
-			name: 'home',
-			component: HomeView,
-		},
-	],
-})
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: HomeView,
+    },
+    {
+      path: "/game/:word",
+      name: "Game",
+      component: Game,
+      props: true,
+    },
+  ],
+});
 
-export default router
+export default router;
